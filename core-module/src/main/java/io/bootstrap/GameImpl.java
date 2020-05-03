8d9ba4953b3cb051f83d721c2dca23b58188b766
Priyobrato Das <priyodas12@gmail.com>
2020-05-03 14:47:22 +0530

    autowire

--- core-module/src/main/java/io/bootstrap/GameImpl.java
+++ core-module/src/main/java/io/bootstrap/GameImpl.java
@@ -2,6 +2,7 @@ package io.bootstrap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -11,6 +12,7 @@ public class GameImpl implements Game {
     private static final Logger log= LoggerFactory.getLogger(GameImpl.class);
 
     //fields
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount=10;
     private int number;
@@ -19,15 +21,15 @@ public class GameImpl implements Game {
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange=true;
-
+/*
     //constrictor
-//    public GameImpl(NumberGenerator numberGenerator) {
-//        this.numberGenerator = numberGenerator;
-//    }
+    public GameImpl(NumberGenerator numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
     //setter
     public void setNumberGenerator(NumberGenerator numberGenerator) {
         this.numberGenerator = numberGenerator;
-    }
+    }*/
 
     //init
     @PostConstruct
